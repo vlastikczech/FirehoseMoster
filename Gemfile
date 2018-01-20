836ea1e29f81d6d8ab1542753d2b9cc642096494
vlastikczech1 <vlastikczech1@gmail.com>
2018-01-20 15:27:32 -0500

    Created a place form

--- Gemfile
+++ Gemfile
@@ -52,6 +52,9 @@ end
 gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
 
+gem 'will_paginate', '>= 3.1'
+gem 'simple_form'
+
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
   
   def index
-    @places = Place.all
+    @places = Place.all.page(params[:page]).per_page(10)
+  end
+  
+  def new
+    @place = Place.new
   end
 end
