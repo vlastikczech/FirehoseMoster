8295aa603fbf658b538a31c16847955ff2166c0d
vlastikczech1 <vlastikczech1@gmail.com>
2018-01-21 19:38:39 -0500

    Revert "Added devise gem and installed it"
    This reverts commit fb051d7ee3a0e4ca9b7033b1bcb055faae0a771f.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
-  
-  config.action_mailer.default_url_options = { host: 'https://monster-vlastik-walker.herokuapp.com' }
-  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
