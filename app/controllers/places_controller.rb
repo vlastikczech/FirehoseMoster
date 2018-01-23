898a441916badd14b3f0303ebcb5bfb8f81d661f
vlastikczech1 <vlastikczech1@gmail.com>
2018-01-23 15:54:55 -0500

    Adjusted user and destroy permissions for users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
     @places = Place.all.page(params[:page]).per_page(10)
@@ -20,16 +20,30 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
   
   def update
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
     @place.update_attributes(place_params)
     redirect_to root_path
   end
   
   def destroy
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
    
