5dcf8336c80ca37c3352245a4b7c5f1b255e1cb3
vlastikczech1 <vlastikczech1@gmail.com>
2018-01-30 01:07:18 -0500

    Adjusted the mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@monsterapp.com"
   
-  def comment_added
-        mail(to: "vlastikczech1@gmail.com",
-         subject: "A comment has been added to your place")
+  
+  
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
