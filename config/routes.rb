e2ba7d80eb01a9d32a84432cb1fdd5a089162b1b
vlastikczech1 <vlastikczech1@gmail.com>
2018-02-01 00:38:14 -0500

    Added a user dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
+  resources :users, only: :show
 end
