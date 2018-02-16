692ce47ad1e8a6e627b3b722439488e17b4bf78c
vlastikczech1 <vlastikczech1@gmail.com>
2018-02-16 02:10:10 -0500

    Added images to be uploaded and displayed

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'devise'
 gem 'geocoder'
 gem "figaro"
 
+gem 'carrierwave', '~> 1.0'
+
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126223707) do
+ActiveRecord::Schema.define(version: 20180215011127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20180126223707) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_comments_on_place_id", using: :btree
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text    "caption"
+    t.integer "place_id"
+    t.string  "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
