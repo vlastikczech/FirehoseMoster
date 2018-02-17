2a324e6c99c9c7969997d0d51a8b40964e096dfa
vlastikczech1 <vlastikczech1@gmail.com>
2018-02-16 20:16:21 -0500

    Added mount_uploaded to place.rb in models

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,4 +8,6 @@ class Place < ApplicationRecord
   
   validates :address, :description, presence: true
   validates :name, length: { minimum: 3} , presence: true
+  
+  mount_uploader :photo, PhotoUploader
 end
